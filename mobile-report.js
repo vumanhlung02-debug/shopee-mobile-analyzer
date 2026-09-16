2c540b38667264e43877ad8d4a4cd3cb9c77bead
vumanhlung02-debug <vumanhlung02@gmail.com>
2026-09-16 23:50:32 +0700

    Update mobile-report.js

--- mobile-report.js
+++ mobile-report.js
@@ -4,8 +4,8 @@
   const APP_ID = 'scm-mobile-analyzer';
   const API_ORIGIN = 'https://affiliate.shopee.vn';
   const PAGE_SIZE = 100;
-  const MAX_ORDER_PAGES = 500;
-  const MAX_CLICK_PAGES = 500;
+  const MAX_ORDER_PAGES = 10000;
+  const MAX_CLICK_PAGES = 100000;
   const SUPPORTED_HOST_RE = /(^|\.)affiliate\.shopee\.vn$/i;
   const state = {
     days: 7,
